d7f181ae4e8ea5fa238f28209ca044aabf85bf1f
Gary Mathews <contact@garymathews.com>
2025-12-27 21:04:33 -0800

    Update CI to only build AMD64

--- docker/rocm/rocm.mk
+++ docker/rocm/rocm.mk
@@ -7,9 +7,11 @@ local-rocm: version
 
 build-rocm: version
 	docker buildx bake --file=docker/rocm/rocm.hcl rocm \
-		--set rocm.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-rocm
+		--set rocm.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-rocm \
+		--set rocm.tags=$(IMAGE_REPO):latest-rocm
 
 push-rocm: build-rocm
 	docker buildx bake --file=docker/rocm/rocm.hcl rocm \
 		--set rocm.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-rocm \
+		--set rocm.tags=$(IMAGE_REPO):latest-rocm \
 		--push
